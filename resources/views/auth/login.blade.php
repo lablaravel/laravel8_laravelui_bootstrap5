42e35e547921c470ebb0d3ca792119ec6685947c
pwcodigo <pwcodigo@gmail.com>
2021-01-26 03:10:12 -0300

    feat: criando tela de login, faltando adicionar helpers do blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,117 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="pt-br">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Criação de dashboard com laravel e bootstrap5">
+	<meta name="author" content="LaravelAdmin">
+	<meta name="keywords" content="laravel, dashboard, bootstrap5">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+	<link rel="canonical" href="#" />
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+	<title>Login | Laravel Dashboard</title>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+	<link href="css/app.css" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+	<!-- BEGIN SETTINGS -->
+	<script src="js/settings.js"></script>
+	<!-- END SETTINGS -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120946860-10"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+  gtag('config', 'UA-120946860-10', { 'anonymize_ip': true });
+</script></head>
+<!--
+  HOW TO USE: 
+  data-theme: default (default), dark, light
+  data-layout: fluid (default), boxed
+  data-sidebar: left (default), right
+-->
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body data-theme="default" data-layout="fluid" data-sidebar="left">
+	<main class="d-flex w-100 h-100">
+		<div class="container d-flex flex-column">
+			<div class="row vh-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+						<div class="text-center mt-4">
+							<h1 class="h2">Seja Bem-vindo</h1>
+							<p class="lead">
+								Acesse a sua conta para continuar
+							</p>
+						</div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+						<div class="card">
+							<div class="card-body">
+								<div class="m-sm-4">
+									<div class="text-center">
+										<img src="/images/avatar/avatar1.png" width="132" height="132" />
+									</div>
+									<form>
+										<div class="mb-3">
+											<label class="form-label">E-mail</label>
+											<input class="form-control form-control-lg" type="email" name="email" placeholder="Digite o e-mail" />
+										</div>
+										<div class="mb-3">
+											<label class="form-label">Senha</label>
+											<input class="form-control form-control-lg" type="password" name="password" placeholder="Digite a sua senha" />
+											<small>
+												<a href="pages-reset-password.html">Esqueceu a senha?</a>
+											</small>
+										</div>
+										<div>
+											<label class="form-check">
+												<input class="form-check-input" type="checkbox" value="remember-me" name="remember-me" checked>
+												<span class="form-check-label">
+													lembrar-me
+												</span>
+											</label>
+										</div>
+										<div class="text-center mt-3">
+											<a href="index.html" class="btn btn-primary btn-sn text-white" role="button">Entrar</a>
+											<!-- <button type="submit" class="btn btn-lg btn-primary">Sign in</button> -->
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+	<script src="js/app.js"></script>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    setTimeout(function(){
+      if(localStorage.getItem('popState') !== 'shown'){
+        window.notyf.open({
+          type: "success",
+          message: "Get access to all 500+ components and 45+ pages with AdminKit PRO. <u><a class=\"text-white\" href=\"https://adminkit.io/pricing\" target=\"_blank\">More info</a></u> 🚀",
+          duration: 10000,
+          ripple: true,
+          dismissible: false,
+          position: {
+            x: "left",
+            y: "bottom"
+          }
+        });
+
+        localStorage.setItem('popState','shown');
+      }
+    }, 15000);
+  });
+</script></body>
+
+</html>a
